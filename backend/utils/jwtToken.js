167b69e7956fe3e051f34025d51e9261b8baeedc
laxman67 <laxmanhenricks94@gmail.com>
2024-10-01 22:49:55 +0530

    jwttoken samesite:none

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,8 +5,9 @@ export const generateToken = (user, message, statusCode, res) => {
     .cookie('token', token, {
       expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000), // 10 days
       httpOnly: true, // Prevents client-side access
+      // Theses Two Lines are mandatory if you're planning to render on third party hpsting like render
       secure: process.env.NODE_ENV === 'production', //Only use HTTPS in production
-      sameSite: 'none', // Allows cross-site requests if needed
+      sameSite: 'none', // Allows cross-site requests if needed✅
     })
     .json({
       success: true,
