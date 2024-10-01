94557c9fba3e41214577122e72bb74689047082d
laxman67 <laxmanhenricks94@gmail.com>
2024-10-01 22:28:15 +0530

    some auth config

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ import ErrorHandler from '../middleware/error.js';
 import jwt from 'jsonwebtoken';
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.cookies.token; // Directly access the token from cookies
 
   if (!token) {
     return next(new ErrorHandler('User is not authenticated', 401));
@@ -14,13 +14,12 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = await User.findById(decoded.id);
 
-    // If user is not Found
+    // If user is not found
     if (!req.user) {
       return next(new ErrorHandler('User not found', 404));
     }
 
-    // actually we're passing req. object in next so that we can extract it out to next middleware
-    next();
+    next(); // Proceed to the next middleware
   } catch (error) {
     return next(new ErrorHandler('Invalid or expired token', 401)); // Handle token errors
   }
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,12 +1,12 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
-
   res
-    .status(statusCode) //Status Code
+    .status(statusCode) // Status Code
     .cookie('token', token, {
-      //set cookie
-      expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
+      expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000), // 10 days
+      httpOnly: true, // Prevents client-side access
+      secure: process.env.NODE_ENV === 'production', //Only use HTTPS in production
+      sameSite: 'none', // Allows cross-site requests if needed
     })
     .json({
       success: true,
@@ -15,3 +15,5 @@ export const generateToken = (user, message, statusCode, res) => {
       user,
     });
 };
+
+// Update due to some problems
